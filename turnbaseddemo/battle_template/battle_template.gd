cebf7d1173dc8def835cbc7c80f14455f30e1ff8
blazetbd <blazetbd@gmail.com>
2025-07-08 14:54:49 -0400

    Start of enemy AI

--- turnbaseddemo/battle_template/battle_template.gd
+++ turnbaseddemo/battle_template/battle_template.gd
@@ -5,8 +5,11 @@ var turn_queue: Array = []
 var curr_unit: Character
 
 var enemies: Array[Node3D] = []
+var party: Array[Node3D] = []
 var current_target_index: int = 0
 var current_target: Node3D = null
+
+var attacking: bool = false
 
 
 func _ready() -> void:
@@ -16,6 +19,8 @@ func _ready() -> void:
 	for character in turn_queue:
 		if character.team == "enemy":
 			enemies.append(character)
+		elif character.team == "party":
+			party.append(character)
 	if enemies.size() > 0:
 		current_target_index = 0
 		current_target = enemies[0]
@@ -27,6 +32,9 @@ func _unhandled_input(event: InputEvent) -> void:
 		cycle_target(1)
 	elif event.is_action_pressed("left"):
 		cycle_target(-1)
+	elif event.is_action_pressed("confirm") and attacking == false:
+		await attack(current_target)
+		end_current_turn()
 
 
 func load_enemies() -> void:
@@ -40,6 +48,11 @@ func load_enemies() -> void:
 	add_child(enemy2)
 	enemy2.team = "enemy"
 	enemy2.global_position = $Enemies/Slot4.global_position
+	
+	var enemy3 = scene.instantiate()
+	add_child(enemy3)
+	enemy3.team = "enemy"
+	enemy3.global_position = $Enemies/Slot1.global_position
 	#print("spawned")
 
 
@@ -58,13 +71,20 @@ func next_turn():
 		turn_queue = get_unit_order()
 		
 	var camera_rig = $CameraRig
+	print("Queue: " + str(turn_queue))
 	curr_unit = turn_queue.pop_front()
 	curr_unit.take_turn()
-	camera_rig.global_position = curr_unit.global_position
-	camera_rig.global_position.y += 2
-	camera_rig.global_position.x += 2
-	camera_rig.target_to_look_at = current_target
-	print(curr_unit.team)
+	
+	if curr_unit.team == "enemy":
+		camera_rig.target_to_look_at = curr_unit
+		await attack(party[0])
+		end_current_turn()
+	else:
+		print("Player turn!")
+		camera_rig.global_position = curr_unit.global_position
+		camera_rig.global_position.y += 2
+		camera_rig.global_position.x += 2
+		camera_rig.target_to_look_at = current_target
 
 func end_current_turn():
 	print("End turn")
@@ -89,3 +109,11 @@ func cycle_target(direction: int):
 	
 	var camera_rig = $CameraRig
 	camera_rig.target_to_look_at = current_target
+
+
+func attack(target) -> void:
+	attacking = true
+	target.health -= curr_unit.attack
+	print(target.team + ": " + str(target.health))
+	await get_tree().create_timer(3).timeout
+	attacking = false
--- turnbaseddemo/character/character.gd
+++ turnbaseddemo/character/character.gd
@@ -5,7 +5,8 @@ class_name Character
 
 
 var team: String
-var health: int = 100
+@export var health: int = 100
+@export var attack: int = 10
 @export var speed: int = 10
 var is_turn: bool = false
 
